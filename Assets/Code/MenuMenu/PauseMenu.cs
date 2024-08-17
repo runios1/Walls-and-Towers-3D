abdd6e544ea53b5227ce1ae89f1c3c37b7b679d1
TheRealWaterFox <70340953+TheRealWaterFox@users.noreply.github.com>
2024-08-17 18:34:31 +0300

    Pause Menu Added
    Pause Menu Added to the main game

---
+++ Assets/Code/MenuMenu/PauseMenu.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    public GameObject pauseMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Game is paused");
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Game is resumed");
+   
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(0);//game scene to change  file->build settings
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
