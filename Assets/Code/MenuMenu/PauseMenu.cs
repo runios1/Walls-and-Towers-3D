5c82f3cab8f4d23e958ea6fe6026fb41b23b4d99
l-habani <70340953+l-habani@users.noreply.github.com>
2024-08-30 19:51:30 +0300

    sound pause on pause

--- Assets/Code/MenuMenu/PauseMenu.cs
+++ Assets/Code/MenuMenu/PauseMenu.cs
@@ -8,6 +8,7 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPause = false;
     public GameObject pauseMenu;
+    public AudioSource audioSourceMusic;
     // Update is called once per frame
     void Update()
     {
@@ -27,6 +28,7 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         Debug.Log("Game is paused");
+        AudioSourceIsEnable(false);
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         GameIsPause = true;
@@ -37,13 +39,35 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         Debug.Log("Game is resumed");
-   
+        AudioSourceIsEnable(true);
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         GameIsPause = false;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
+    private void AudioSourceIsEnable(bool enable)
+    {
+        // Get all AudioSource components in the scene
+        AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (enable)
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Pause();
+            }
+        }
+        audioSourceMusic.Play();
+    }
+
+
+
+
     public void LoadMenu()
     {
         Time.timeScale = 1f;
